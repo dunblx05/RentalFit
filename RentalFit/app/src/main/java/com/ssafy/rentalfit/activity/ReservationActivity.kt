521f86e2d3e180c8c6c4196ed80effa31fc03741
hyuun <137624438+hyuun@users.noreply.github.com>
2024-11-20 17:34:20 +0900

    Add Flow  Fragments on ReservationActivity

--- RentalFit/app/src/main/java/com/ssafy/rentalfit/activity/ReservationActivity.kt
+++ RentalFit/app/src/main/java/com/ssafy/rentalfit/activity/ReservationActivity.kt
@@ -2,22 +2,52 @@ package com.ssafy.rentalfit.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.ssafy.rentalfit.R
 import com.ssafy.rentalfit.base.BaseActivity
 import com.ssafy.rentalfit.databinding.ActivityReservationBinding
+import com.ssafy.rentalfit.ui.cart.CartFragment
+import com.ssafy.rentalfit.ui.equip.EquipDetailFragment
 import com.ssafy.rentalfit.ui.place.PlaceDetailFragment
+import com.ssafy.rentalfit.ui.place.PlaceFragment
 
 class ReservationActivity : BaseActivity<ActivityReservationBinding>(ActivityReservationBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        initView()
-        // 여기에 액티비티 초기화 및 UI 작업 추가
+
+        // 기본값으로 PlaceDetail이 열리도록 설정.
+        val name = intent.getStringExtra("name") ?: "PlaceDetail"
+
+        changeFragmentReservation(name)
     }
 
-    fun initView(){
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragmentContainer, PlaceDetailFragment()) // fragmentContainer는 FrameLayout의 id
-            .commit()
+    private fun changeFragmentReservation(name: String) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        var goto: Fragment = PlaceDetailFragment()
+
+        when(name) {
+
+            // 장바구니 화면
+            "Cart" -> {
+                goto = CartFragment()
+            }
+
+            // 장소 상세 화면
+            "PlaceDetail" -> {
+                goto = PlaceDetailFragment()
+            }
+
+            // 장비 상세 화면
+            "EquipDetail" -> {
+                goto = EquipDetailFragment()
+            }
+        }
+
+        transaction.replace(R.id.fragmentContainer, goto)
+        transaction.addToBackStack(name)
+        transaction.commit()
     }
 }
---
+++ RentalFit/app/src/main/java/com/ssafy/rentalfit/ui/cart/CartFragment.kt
@@ -0,0 +1,9 @@
+package com.ssafy.rentalfit.ui.cart
+
+import com.ssafy.rentalfit.R
+import com.ssafy.rentalfit.base.BaseFragment
+import com.ssafy.rentalfit.databinding.FragmentCartBinding
+
+class CartFragment: BaseFragment<FragmentCartBinding>(FragmentCartBinding::bind, R.layout.fragment_cart) {
+
+}

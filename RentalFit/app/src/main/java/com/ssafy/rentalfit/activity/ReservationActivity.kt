c9525db0dd7e5878fff1f63398aa9b4ae08a3a89
sunwoo <ghi03082@naver.com>
2024-11-20 14:03:59 +0900

    Place Reservation Blank init

--- RentalFit/app/src/main/java/com/ssafy/rentalfit/activity/ReservationActivity.kt
+++ RentalFit/app/src/main/java/com/ssafy/rentalfit/activity/ReservationActivity.kt
@@ -1,19 +1,23 @@
 package com.ssafy.rentalfit.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.ssafy.rentalfit.R
-import com.ssafy.rentalfit.databinding.ActivityReservationBinding
 import com.ssafy.rentalfit.base.BaseActivity
+import com.ssafy.rentalfit.databinding.ActivityReservationBinding
+import com.ssafy.rentalfit.ui.place.PlaceDetailFragment
 
 class ReservationActivity : BaseActivity<ActivityReservationBinding>(ActivityReservationBinding::inflate) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        initView()
         // 여기에 액티비티 초기화 및 UI 작업 추가
+    }
+
+    fun initView(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlaceDetailFragment()) // fragmentContainer는 FrameLayout의 id
+            .commit()
     }
 }
--- RentalFit/app/src/main/java/com/ssafy/rentalfit/ui/place/PlaceDetailFragment.kt
+++ RentalFit/app/src/main/java/com/ssafy/rentalfit/ui/place/PlaceDetailFragment.kt
@@ -4,17 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.rentalfit.R
 import com.ssafy.rentalfit.base.BaseFragment
 import com.ssafy.rentalfit.databinding.FragmentPlaceDetailBinding
 
 class PlaceDetailFragment : BaseFragment<FragmentPlaceDetailBinding>(
     bind = { view -> FragmentPlaceDetailBinding.bind(view) },
-    layoutResId = R.layout.fragment_equip
+    layoutResId = R.layout.fragment_place_detail
 ) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // 앱바 제목 설정
+//        activity?.actionBar?.apply {
+//            title = "장소 대여"
+//        }
+
+        // 예시로 장소 데이터 설정
+        val placeImage = R.drawable.sample_place_image // 이미지 파일
+        val placeLocation = "서울 강남구"
+        val placeCapacity = "최대 100명"
+        val placeDescription = "이 장소는 축구 경기를 위한 시설로, 넓은 운동장이 마련되어 있습니다."
+
+        // 데이터를 뷰에 설정
+        binding.apply {
+            imageViewPlace.setImageResource(placeImage)
+            textViewLocation.text = placeLocation
+            textViewCapacity.text = placeCapacity
+            textViewDescription.text = placeDescription
+        }
     }
 }

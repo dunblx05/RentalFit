2e4a607923fe2f971c5c43ffcd7287b2f1c7e4b9
hyuun <137624438+hyuun@users.noreply.github.com>
2024-11-22 11:45:13 +0900

    [Modify UI] EquipDetailFragment

--- RentalFit/app/src/main/java/com/ssafy/rentalfit/ui/equip/EquipDetailFragment.kt
+++ RentalFit/app/src/main/java/com/ssafy/rentalfit/ui/equip/EquipDetailFragment.kt
@@ -15,8 +15,6 @@ class EquipDetailFragment : BaseFragment<FragmentEquipDetailBinding>(FragmentEqu
 
     private lateinit var reservationActivity: ReservationActivity
 
-    private lateinit var equipDetailAdapter: EquipDetailAdapter
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         reservationActivity = context as ReservationActivity
@@ -25,21 +23,9 @@ class EquipDetailFragment : BaseFragment<FragmentEquipDetailBinding>(FragmentEqu
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        settingRecyclerView()
         settingEvent()
     }
 
-    // 리사이클러뷰 설정
-    private fun settingRecyclerView() {
-
-        equipDetailAdapter = EquipDetailAdapter()
-
-        binding.apply {
-
-            recyclerViewEquipDetail.layoutManager = LinearLayoutManager(reservationActivity, LinearLayoutManager.VERTICAL, false)
-            recyclerViewEquipDetail.adapter = equipDetailAdapter
-        }
-    }
 
     // 이벤트 설정
     private fun settingEvent() {
@@ -49,13 +35,6 @@ class EquipDetailFragment : BaseFragment<FragmentEquipDetailBinding>(FragmentEqu
             // 뒤로 가기
             imageEquipDetailBack.setOnClickListener {
                 reservationActivity.finish()
-            }
-
-            // 장바구니에 추가하기.
-            equipDetailAdapter.equipDetailAddListener = object : EquipDetailAdapter.ItemClickListener {
-                override fun onClick() {
-
-                }
             }
 
             // 장바구니로 가기.
